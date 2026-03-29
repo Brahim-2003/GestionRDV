59ab36a9c50364ef23099bbc74fe8e331fb71e6d
Brahim Zakaria <brahimzakariabrahimbzb@gmail.com>
2026-03-29 11:03:39 +0100

    mise a jour

---
+++ GestionRDV/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+handler403 = 'users.views.permission_denied_view'
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("users/", include('users.urls')),
+    path("rdv/", include('rdv.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
